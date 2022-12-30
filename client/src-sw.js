0f596e550f631619a06337997f469379d3599e46
404pandas <113853550+404pandas@users.noreply.github.com>
2022-12-29 23:11:23 -0500

    Built out asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,3 +1,9 @@
+// Caching js and css requires workbox-strategies to be installed
+// To actually respond to requests with a cached response,
+// we need to use a strategy called StaleWhileRevalidate
+// This strategy will first check the cache for a response, and if it finds one, it will return it.
+
+
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
@@ -5,6 +11,8 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// The precacheAndRoute() method takes an array of URLs to precache.
+// The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -26,5 +34,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Asset caching
+registerRoute(
+    // Defines the callback function that will filter the requests to cache
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      // Name of the cache storage.
+      cacheName: 'asset-cache',
+      plugins: [
+        // This plugin will cache responses with these headers to a maximum-age of 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    })
+);
